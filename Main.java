d8dcdad287d91d9b3ae9ffc960d186990547111b
niclesanti <niclesanti687@gmail.com>
2024-09-27 17:04:18 -0300

    Se agrego un comentario A en la clase Main

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public class Main {
         //Agregamos algunos print para la consigna d
         System.out.println("Ejemplo para TP de Ingenieria y Calidad de software.");
         System.out.println("Nueva version 1.0.1\n");
+        //Cambio A
         
         // Agregar algunos libros a la biblioteca
         biblioteca.agregarLibro(new Libro("Cien años de soledad", "Gabriel García Márquez"));
